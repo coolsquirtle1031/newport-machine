6a7c65a6cc90ff15544ef61973611d4698c540b4
coolsquirtle1031 <jaehyunjamesent@gmail.com>
2025-02-08 17:36:43 -0600

    added when atleast 1 memo

---
+++ client/src/components/ActiveMemo.tsx
@@ -0,0 +1,13 @@
+import "./ActiveMemo.css";
+
+export default function ActiveMemo() {
+  return (
+    <div className="active-memo">
+      <div className="active-memo-top">
+        <div className="active-memo-title">Title</div>
+        <div className="active-memo-date">October 31, 2001</div>
+      </div>
+      <div className="active-memo-bottom">To me, the deep life is about</div>
+    </div>
+  );
+}
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,6 +1,9 @@
+import ActiveMemo from "../components/ActiveMemo";
 import "./HomePage.css";
 
 export default function HomePage() {
+  const active = 1;
+
   return (
     <div className="home-page">
       <div className="home-page-top">
@@ -9,11 +12,24 @@ export default function HomePage() {
       </div>
       <div className="home-page-content-container">
         <div className="home-page-content">
-          <div className="home-page-content-active-title">Active</div>
-          <div className="home-page-content-no-active-memo">
-            <div>You have no active memo yet! </div>
-            <div>Click the + button to add one</div>
-          </div>
+          {active === 0 && (
+            <div className="home-page-content-no-active-memo">
+              <div>You have no active memo yet! </div>
+              <div>Click the + button to add one</div>
+            </div>
+          )}
+          {active > 0 && (
+            <>
+              <div className="home-page-content-active-title">Active</div>
+              <div className="home-page-content-active-memo-container">
+                <ActiveMemo />
+                <ActiveMemo />
+                <ActiveMemo />
+                <ActiveMemo />
+                <ActiveMemo />
+              </div>
+            </>
+          )}
         </div>
       </div>
       <button className="home-page-add-button">+</button>
