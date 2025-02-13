98b6fcef982f1ce260b905fc2c3fc8bbb3448d28
coolsquirtle1031 <jaehyunjamesent@gmail.com>
2025-02-12 21:00:33 -0600

    initial work on profile

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import ActiveMemo from "../components/ActiveMemo";
 import "./HomePage.css";
 import { Memo } from "../types/memo";
+import squirtlePath from "../images/squirtle.png";
 
 // http://localhost:5001
 // https://393e-2600-1700-74-290-c558-8d86-fd4-846b.ngrok-free.app
@@ -28,7 +29,9 @@ export default function HomePage() {
     <div className="home-page">
       <div className="home-page-top">
         <div className="home-page-top-page">Home</div>
-        <div className="home-page-top-profile">James</div>
+        <div className="home-page-top-profile">
+          <img src={squirtlePath} />
+        </div>
       </div>
       <div className="home-page-content-container">
         <div className="home-page-content">
